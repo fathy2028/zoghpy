9aa9227ad21c8bd815916620e6d8dd6f60cdd236
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-09-03 17:10:27 +0300

    fff

--- client/src/components/Layout/Myheader.js
+++ client/src/components/Layout/Myheader.js
@@ -6,13 +6,13 @@ import toast from "react-hot-toast";
 import SearchInput from '../Form/SearchInput';
 import useCategory from '../../hooks/useCategory';
 import { useCart } from '../../context/cart';
-import {Badge} from "antd"
+import { Badge } from "antd";
 
 const Myheader = () => {
     const [auth, setAuth] = useAuth();
     const categories = useCategory();
-    const [cart,setCart]=useCart();
-    
+    const [cart, setCart] = useCart();
+
     const handleLogout = () => {
         setAuth({
             ...auth,
@@ -23,78 +23,102 @@ const Myheader = () => {
         toast.success("تم تسجيل الخروج بنجاح");
     };
 
+    const styles = {
+        navbar: {
+            backgroundColor: '#f8f9fa',
+            borderBottom: '1px solid #e0e0e0',
+            padding: '10px 20px',
+            fontFamily: 'Tajawal, sans-serif',
+        },
+        brand: {
+            color: '#333',
+            fontSize: '24px',
+            fontWeight: 'bold',
+            letterSpacing: '1px',
+        },
+        navLink: {
+            color: '#333',
+            margin: '0 10px',
+            fontSize: '16px',
+        },
+        dropdownToggle: {
+            cursor: 'pointer',
+        },
+        badge: {
+            cursor: 'pointer',
+        }
+    };
+
     return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarTogglerDemo01"
-                        aria-controls="navbarTogglerDemo01"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link to="/" className="navbar-brand">الزغبي للاثاث</Link>
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <SearchInput />
-                            <li className="nav-item">
-                                <NavLink to="/" className="nav-link">الرئيسية</NavLink>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link  className="nav-link dropdown-toggle"  data-bs-toggle="dropdown" >
-                                    الفئات
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    {categories?.map(c => (
-                                        <li key={c._id}><Link to={`/category/${c._id}`} className="dropdown-item">{c.name}</Link></li>
-                                    ))}
-                                </ul>
-                            </li>
-                            {!auth.user ? (
-                                <>
-                                    <li className="nav-item">
-                                        <NavLink to="/register" className="nav-link">انشاء حساب</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink to="/login" className="nav-link">تسجيل الدخول</NavLink>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    <li className="nav-item dropdown">
-                                        <NavLink className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            {auth?.user?.name}
-                                        </NavLink>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <NavLink to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="dropdown-item">
-                                                    لوحة التحكم
-                                                </NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink to="/login" onClick={handleLogout} className="dropdown-item">
-                                                    تسجيل الخروج
-                                                </NavLink>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </>
-                            )}
-                            <li className="nav-item">
-                               <Badge count={cart?.length} showZero>
-                               <NavLink to="/cart" className="nav-link"><GiShoppingBag /> عربة الشراء</NavLink>
-                               </Badge>
-                            </li>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg" style={styles.navbar}>
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <Link to="/" className="navbar-brand" style={styles.brand}>الزغبي للاثاث</Link>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <SearchInput />
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link" style={styles.navLink}>الرئيسية</NavLink>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" style={styles.dropdownToggle} data-bs-toggle="dropdown">
+                                الفئات
+                            </Link>
+                            <ul className="dropdown-menu">
+                                {categories?.map(c => (
+                                    <li key={c._id}><Link to={`/category/${c._id}`} className="dropdown-item">{c.name}</Link></li>
+                                ))}
+                            </ul>
+                        </li>
+                        {!auth.user ? (
+                            <>
+                                <li className="nav-item">
+                                    <NavLink to="/register" className="nav-link" style={styles.navLink}>انشاء حساب</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/login" className="nav-link" style={styles.navLink}>تسجيل الدخول</NavLink>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item dropdown">
+                                    <NavLink className="nav-link dropdown-toggle" style={styles.dropdownToggle} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {auth?.user?.name}
+                                    </NavLink>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <NavLink to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="dropdown-item">
+                                                لوحة التحكم
+                                            </NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink to="/login" onClick={handleLogout} className="dropdown-item">
+                                                تسجيل الخروج
+                                            </NavLink>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </>
+                        )}
+                        <li className="nav-item">
+                            <Badge count={cart?.length} showZero style={styles.badge}>
+                                <NavLink to="/cart" className="nav-link" style={styles.navLink}><GiShoppingBag /> عربة الشراء</NavLink>
+                            </Badge>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 };
 
