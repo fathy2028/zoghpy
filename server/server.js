a5cd66134d064867a4f9aea434e532cf6b4bbe48
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-09-03 15:25:34 +0300

    hyvtt

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.green);
